a822beefeff41dc5ef45691811ab891bab60fdf1
vasylShd <vasiliy.shadura@gmail.com>
2023-02-17 19:45:37 +0200

    task1 of lesson2 - done

---
+++ Homework/lesson2_shadura.py
@@ -0,0 +1,23 @@
+# task 1
+name = input('Enter your name: ')
+last_name = input('Enter your lastname: ')
+# name = 'vasyl'
+# last_name = 'shadura'
+
+full_name = name + ' ' + last_name
+print('Personal data: ', full_name)
+
+full_name = name.capitalize() + ' ' + last_name.capitalize()
+print(full_name)
+
+print(name * 5)
+
+name = '\t' + name + '\t\n'
+print(name)
+name = name.strip()
+print(name)
+
+
+
+
+
