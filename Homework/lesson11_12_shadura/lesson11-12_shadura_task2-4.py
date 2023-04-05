7f80ae53cae62cd72633b83e014e178da899c23e
vasylShd <vasiliy.shadura@gmail.com>
2023-04-06 00:45:21 +0300

    lesson11-12 is done

--- Homework/lesson11_12_shadura/lesson11-12_shadura_task2-4.py
+++ Homework/lesson11_12_shadura/lesson11-12_shadura_task2-4.py
@@ -22,12 +22,14 @@ finally:
 # task 3
 print('\ntask3')
 
+
 def diff_date(date_in, days, plus):
     '''Function that adds or subtracts a certain number of days from a given date'''
     if plus == '+':
         return date_in + datetime.timedelta(days=days)
     else:
         return date_in - datetime.timedelta(days=days)
+
 
 form_data = '%Y.%m.%d %H:%M'
 dat = input(f'Enter date in format "{form_data}": ')
@@ -41,10 +43,13 @@ print(diff_date(dat, days, plus_minus).strftime('%Y.%m.%d %H:%M'))
 # task 4
 print('task 4')
 
+
 def age(first_day):
+    '''function that calculates exact age'''
     first_day_stamp = first_day.timestamp()
     now = datetime.datetime.now().timestamp()
     return (now - first_day_stamp)
+
 
 form_data = '%Y.%m.%d'
 birthday = input(f'Enter date in format "{form_data}": ')
