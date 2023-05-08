8639b0e257c5c98828980e4686ac816072543858
vasylShd <vasiliy.shadura@gmail.com>
2023-05-08 17:02:26 +0300

    hw 18 lesson 22 is done

---
+++ Homework/hw18_lesson22_shadura/test_processing_DokUa.py
@@ -0,0 +1,67 @@
+from selenium.webdriver import Chrome, Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+import time
+import pytest
+
+def test_dokUa():
+    service = Service('chromedriver/chromedriver')
+    driver = Chrome(service=service)
+    start_page = 'https://dok.ua/ua/doc/car/12007'
+    try:
+        driver.get(start_page)
+        driver.maximize_window()
+        wait = WebDriverWait(driver, 10)  # реалізація явного очікування
+
+        # вибір року випуску авто
+        select_manufacture_year = '//div[@class="fva__table fva__table_visible"]/div[3]/div[9]'
+        wait.until(EC.element_to_be_clickable((By.XPATH, select_manufacture_year)))  # явне очікування
+        search_year = driver.find_element(By.XPATH, select_manufacture_year).click()
+
+        # вибір поля для пошуку автозапчастини
+        search_input_field_locator = '// *[ @ id = "search_with-hints"]'
+        wait.until(EC.presence_of_element_located((By.XPATH, search_input_field_locator))) # явне очікування
+        select_input_field = driver.find_element(By.XPATH, search_input_field_locator)
+
+        # ввід данних для пошуку і перехід на потрібну сторінку
+        select_input_field.send_keys('Гальмівні колодки')
+        press_selected_data = '//div[@class="search-wrap opened"]/ul[1]/li/a'
+        wait.until(EC.element_to_be_clickable((By.XPATH, press_selected_data)))  # явне очікування
+        driver.find_element(By.XPATH, press_selected_data).click()
+
+        # встановлення у фільтрі мін ціни (750)
+        price_min_field = '//div[@class="filter__list"]/div/input[@class="from"]'
+        wait.until(EC.presence_of_element_located((By.XPATH, price_min_field)))  # явне очікування
+        input_price_min = driver.find_element(By.XPATH, price_min_field)
+        input_price_min.clear()
+        input_price_min.send_keys(750)
+        time.sleep(3)
+        assert input_price_min.get_attribute('value') == '750', "Error in test 1" # перевірка правильності вводу данних
+
+        # встановлення у фільтрі макс ціни (12500)
+        price_max_field = '//div[@class="filter__list"]/div/input[@class="to"]'
+        wait.until(EC.presence_of_element_located((By.XPATH, price_max_field)))  # явне очікування
+        input_price_max = driver.find_element(By.XPATH, price_max_field)
+        input_price_max.clear()
+        input_price_max.send_keys(12500)
+
+        # підтердження вибору
+        button_ok = '//div[@class="filter__list"]/div/input[@class="ok"]'
+        wait.until(EC.element_to_be_clickable((By.XPATH, button_ok)))             # явне очікування
+        assert driver.find_element(By.XPATH, button_ok).is_enabled() == True, "Error in test 2" #перевірка знаходж єлем
+        press_ok = driver.find_element(By.XPATH, button_ok).click()
+
+        # пагінація. перехід на сторінку №2
+        page_2 = '//div[@class="catalog__goods-column-pane"]/div[2]/ul/li[2]'
+        wait.until(EC.element_to_be_clickable((By.XPATH, page_2)))               # явне очікування
+        driver.find_element(By.XPATH, page_2).click()
+        time.sleep(10)
+
+    except Exception as ex:
+        print(ex)
+
+    finally:
+        driver.close()
+        driver.quit()
